acbe9cab726cd97900cde238c95521bec49c638c
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-04-01 00:38:40 +0200

    add translations

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {DashboardComponent} from "./features/dashboard/dashboard.component";
+import { DashboardComponent } from "./features/dashboard/dashboard.component";
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DashboardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    DashboardComponent,
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Sandro SPINA - CV';
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.addLangs(['en', 'fr']);
+    this.translateService.setDefaultLang('fr');
+    this.translateService.use('fr');
+  }
 }
--- front/src/app/features/dashboard/header/header.component.ts
+++ front/src/app/features/dashboard/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private _currentLang: string = '';
 
+  constructor(private translateService: TranslateService) {
+    this._currentLang = this.translateService.currentLang;
+  }
+
+  get currentLang(): string {
+    return this._currentLang;
+  }
+
+  changeLang(lang: string) {
+    this.translateService.use(lang);
+    this._currentLang = lang;
+  }
 }
