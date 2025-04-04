c4f1d7be6b50943113f8ea57acf763a0f51bdff3
SandroSpina698 <sandro.spina.pro@gmail.com>
2025-04-04 16:01:13 +0200

    add texts

--- front/src/app/shared/components/shared-body-text/shared-body-text.component.ts
+++ front/src/app/shared/components/shared-body-text/shared-body-text.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {TranslatePipe} from "@ngx-translate/core";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-shared-body-text',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslatePipe,
+    NgIf
+  ],
   templateUrl: './shared-body-text.component.html',
   styleUrl: './shared-body-text.component.scss'
 })
 export class SharedBodyTextComponent {
+  private _bulletList: string[] = [];
+  hasText: boolean = true;
+  hasBulletList: boolean = false;
 
+  @Input() textKey: string = '';
+  @Input() set bulletList(newBulletList: string[]) {
+    this._bulletList = newBulletList;
+    if (newBulletList.length > 0) {
+      this.textKey = '';
+      this.hasText = false;
+      this.hasBulletList = true;
+    }
+  };
+
+  get bulletList(): string[] {
+    return this._bulletList;
+  }
 }
